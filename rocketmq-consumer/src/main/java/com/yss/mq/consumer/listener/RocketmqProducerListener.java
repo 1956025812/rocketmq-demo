8d7b5238d54f1f470eefe0e17b4e666c53e7588b
杜学博 <xuebo.du@zhenai.com>
2020-08-10 19:46:17 +0800

    不需要做ACK

--- rocketmq-consumer/src/main/java/com/yss/mq/consumer/listener/RocketmqProducerListener.java
+++ rocketmq-consumer/src/main/java/com/yss/mq/consumer/listener/RocketmqProducerListener.java
@@ -1,11 +1,11 @@
 package com.yss.mq.consumer.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.consumer.listener.ConsumeReturnType;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.core.RocketMQReplyListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,17 +19,11 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-@RocketMQMessageListener(consumerGroup = "rocketmq-producer", topic = "rocketmq-producer2", consumeMode = ConsumeMode.ORDERLY)
-public class RocketmqProducerListener implements RocketMQReplyListener<MessageExt, ConsumeReturnType> {
+@RocketMQMessageListener(consumerGroup = "rocketmq-producer", topic = "rocketmq-producer", consumeMode = ConsumeMode.CONCURRENTLY)
+public class RocketmqProducerListener implements RocketMQListener<MessageExt> {
 
     @Override
-    public ConsumeReturnType onMessage(MessageExt messageExt) {
-//        if(messageExt.getReconsumeTimes() == 3) {
-//            log.info("重试打到3次，不再重试, 自己做操作处理");
-//            return;
-//        }
-//        System.out.println(1/0);
+    public void onMessage(MessageExt messageExt) {
         log.info("收到的message对象为：{}， body为：{}", messageExt.toString(), new String(messageExt.getBody()));
-        return ConsumeReturnType.SUCCESS;
     }
 }
